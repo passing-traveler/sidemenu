204025a561066fc1d3e88bbac7008f7b7c6f3482
Abel Dimas <avtw37@gmail.com>
2021-01-11 10:26:36 -0800

    sidebar action

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import * as FaIcons from "react-icons/fa";
@@ -14,16 +14,49 @@ const Nav = styled.div`
 
 const NavIcon = styled(Link)`
   margin-left: 2rem;
+  font-size: 2rem;
+  height: 80px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const SidebarNav = styled.nav`
+  background: #15171c;
+  width: 250px;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: ${({ sidebar }) => (sidebar ? "0" : "-100%")};
+  transition: 350ms;
+  z-index: 10;
+`;
+
+const SidebarWrap = styled.div`
+  width: 100%;
 `;
 
 const Sidebar = () => {
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
+
   return (
     <>
       <Nav>
         <NavIcon to="#">
-          <FaIcons.FaBars />
+          <FaIcons.FaBars onClick={showSidebar} />
         </NavIcon>
       </Nav>
+      <SidebarNav>
+        <SidebarWrap>
+          <NavIcon to="#">
+            <AiIcons.AiOutlineClose />
+          </NavIcon>
+        </SidebarWrap>
+      </SidebarNav>
     </>
   );
 };
