b2197fd92f01a93f3223063d4a4b9f1d7f992a87
Abel Dimas <avtw37@gmail.com>
2021-01-29 22:17:12 -0800

    kick

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import { FaRoad } from "react-icons/fa";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Overview from "./pages/Overview";
+import { Reports, ReportsOne, ReportsTwo, ReportsThree } from "./pages/Reports";
 
 function App() {
   return (
     <Router>
       <Sidebar />
+      <Switch>
+        <Route path="/overview" exact component={Overview} />
+        <Route path="/reports" exact component={Reports} />
+        <Route path="/reports/reports1" exact component={ReportsOne} />
+        <Route path="/reports/reports2" exact component={ReportsTwo} />
+        <Route path="/reports/reports3" exact component={ReportsThree} />
+      </Switch>
     </Router>
   );
 }
